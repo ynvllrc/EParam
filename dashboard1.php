6966347cd4ce18204cb958775fa3025b3671d4e9
Laxus <ianjaspervillarico@gmail.com>
2023-05-30 21:45:36 +0800

    first commit

---
+++ dashboard1.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html lang= "en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
+	<title>Municipal Ordinance</title>
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<input type="checkbox" id="nav-toggle">
+
+<div class="sidebar">
+	<div class="sidebar-brand">
+		<h2><span class="lab la-accusoft"></span><span>Ordinance</span></h2>
+	</div>
+
+<div class="sidebar-menu">
+	<ul>
+		    <li>
+			<a href="" class="active"><span class="las la-igloo"></span>
+				<span>Dashboard</span></a>
+			</li>
+			<li>
+			<a href=""><span class="las la-users"></span>
+				<span>Total Users</span></a>
+			</li>
+			<li>
+			<a href=""><span class="las la-clipboard-list"></span>
+				<span>Total Ordinances</span></a>
+			</li>
+			<li>
+			<a href=""><span class="las la-clipboard-list"></span>
+				<span>Total pending Ordinances</span></a>
+			</li>
+			<li>
+			<a href=""><span class="las la-receipt"></span>
+				<span>Total Vetoed Ordinances</span></a>
+			</li>
+			<li>
+			<a href=""><span class="las la-user-circle"></span>
+				<span>Total Approved Ordinances</span></a>
+			</li>
+
+		</ul>
+	</div>
+</div>
+
+<div class="main-content">
+	<header>
+		<h1>
+			<label for="nav-toggle">
+				<span class="las la-bars"><span>
+				</label>
+
+				Dashboard
+			</h1>
+
+			<div class="search-wrapper">
+				<span class="las la-search"></span>
+				<input type="search" placeholder="Search here">
+			</div>
+
+			<div class="user-wrapper">
+				<img src="img/" width="40px" height="40px" alt="">
+				<div>
+					<h4>John Doe</h4>
+					<small>Super admin</small>
+			</div>
+		</header>
+<main>
+	<div class="cards">
+		<div class="card-single">
+			<div>
+          <h1>54</h1>
+          <span>Provinced</span>
+			</div>
+			<div>
+          <span class="las la-users"></span>
+			</div>
+			</div>
+			<div class="card-single">
+			<div>
+          <h1>79</h1>
+          <span>Municipalities</span>
+			</div>
+			<div>
+          <span class="las la-clipboard-list"></span>
+			</div>
+			</div>
+			<div class="card-single">
+			<div>
+          <h1>124</h1>
+          <span>Barangay</span>
+			</div>
+			<div>
+          <span class="las la-shopping-bag"></span>
+			</div>
+			</div>
+			<div class="card-single">
+			<div>
+          <h1>6k</h1>
+          <span>Population</span>
+			</div>
+			<div>
+          <span class="lab la-google-wallet"></span>
+			</div>
+			</div>
+			</div>
+<div class="recent-grid">
+	<div class="projects">
+         <div class="card">
+         	<div class="card-header">
+             <h3>Recent Projects</h3>
+             <button>See all<span class="las la-arrow-right"></span></button>
+
+         	</div>
+         	<div class="card-body">
+         		<div class="table-responsive">
+         		<table width="100%">
+         			<thead>
+         				<tr>
+         					<td>Project Title</td>
+         					<td>Department</td>
+         					<td>Status</td>
+         				</tr>
+         			</thead>
+                 <tbody>
+                 	<tr>
+                 		<td>UI/UX Design</td>
+                 		<td>UI Team</td>
+                 		<td>
+                 			<span class="status purple"></span>
+                            review
+                 		</td>
+                 	</tr>
+                 	<tr>
+                 		<td>Web development</td>
+                 		<td>FrontEnd</td>
+                 		<td>
+                 			<span class="status pink"></span>
+                            in progress
+                 		</td>
+                 	</tr>
+                 	<tr>
+                 		<td>Ushop app</td>
+                 		<td>Mobile Team</td>
+                 		<td>
+                 			<span class="status orange"></span>
+                            pending
+                 		</td>
+                 	</tr>
+                 </tbody>
+         		</table>
+         	</div>
+         </div>
+	</div>
+</div>
+	<div class="customers">
+		
+			<div></div>
+		   <div class="card">
+         	<div class="card-header">
+             <h3>New customer</h3>
+             <button>See all<span class="las la-arrow-right"></span></button>
+
+         	</div>
+         	<div class="card-body">
+         		<div class="table-responsive">
+         		</div>
+         		<div class="customer">
+
+         			<div class="info">
+         			<img src="img/" width="40px" height="40px"alt="">
+         			<div>
+                     <h4>Lewis S. Cunningham</h4>
+                     <small>CEO Excerpt</small>
+         			</div>
+</div>
+<div class="contact">
+	<span class="las la-user-circle"></span>
+	<span class="las la-comment"></span>
+	<span class="las la-phone"></span>
+</div>
+         		</div>
+         		  		<div class="customer">
+
+         			<div class="info">
+         			<img src="img/" width="40px" height="40px"alt="">
+         			<div>
+                     <h4>Lewis S. Cunningham</h4>
+                     <small>CEO Excerpt</small>
+         			</div>
+</div>
+<div class="contact">
+	<span class="las la-user-circle"></span>
+	<span class="las la-comment"></span>
+	<span class="las la-phone"></span>
+</div>
+         		</div>
+         		  		<div class="customer">
+
+         			<div class="info">
+         			<img src="img/" width="40px" height="40px"alt="">
+         			<div>
+                     <h4>Lewis S. Cunningham</h4>
+                     <small>CEO Excerpt</small>
+         			</div>
+</div>
+<div class="contact">
+	<span class="las la-user-circle"></span>
+	<span class="las la-comment"></span>
+	<span class="las la-phone"></span>
+</div>
+         		</div>  		<div class="customer">
+
+         			<div class="info">
+         			<img src="img/" width="40px" height="40px"alt="">
+         			<div>
+                     <h4>Lewis S. Cunningham</h4>
+                     <small>CEO Excerpt</small>
+         			</div>
+</div>
+<div class="contact">
+	<span class="las la-user-circle"></span>
+	<span class="las la-comment"></span>
+	<span class="las la-phone"></span>
+</div>
+         		</div>
+         		  		<div class="customer">
+
+         			<div class="info">
+         			<img src="img/" width="40px" height="40px"alt="">
+         			<div>
+                     <h4>Lewis S. Cunningham</h4>
+                     <small>CEO Excerpt</small>
+         			</div>
+</div>
+<div class="contact">
+	<span class="las la-user-circle"></span>
+	<span class="las la-comment"></span>
+	<span class="las la-phone"></span>
+</div>
+         		</div>
+         		  		<div class="customer">
+
+         			<div class="info">
+         			<img src="img/" width="40px" height="40px"alt="">
+         			<div>
+                     <h4>Lewis S. Cunningham</h4>
+                     <small>CEO Excerpt</small>
+         			</div>
+</div>
+<div class="contact">
+	<span class="las la-user-circle"></span>
+	<span class="las la-comment"></span>
+	<span class="las la-phone"></span>
+</div>
+         		</div>
+         		  		<div class="customer">
+
+         			<div class="info">
+         			<img src="img/" width="40px" height="40px"alt="">
+         			<div>
+                     <h4>Lewis S. Cunningham</h4>
+                     <small>CEO Excerpt</small>
+         			</div>
+</div>
+<div class="contact">
+	<span class="las la-user-circle"></span>
+	<span class="las la-comment"></span>
+	<span class="las la-phone"></span>
+</div>
+         		</div>
+         	
+</main>
+</div>
+	</body>
+	</html>
